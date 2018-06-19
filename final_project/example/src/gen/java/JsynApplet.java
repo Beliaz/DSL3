c8b83ee5bf2d3ede9a6dfc0ffb36919de60f2900
KoppDaniel <Daniel.Kopp@student.uibk.ac.at>
2018-06-19 17:12:29 +0200

    V7

--- final_project/example/src/gen/java/JsynApplet.java
+++ final_project/example/src/gen/java/JsynApplet.java
@@ -1,131 +1,159 @@
-package gen.java;
+Circuits{
+	"circuitA" (
 
-import com.jsyn.ports.UnitInputPort;
-import com.jsyn.ports.UnitOutputPort;
-import com.jsyn.swing.JAppletFrame;
-import com.jsyn.unitgen.*;
-import main.java.*;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class JsynApplet extends AppletBase {
-
-	public static void main(String args[]) {
-		JsynApplet applet = new JsynApplet();
-		JAppletFrame frame = new JAppletFrame(
-		        "My Synthesizer", applet
-		);
-		frame.setSize(1280, 960);
-		frame.setVisible(true);
-		frame.test();
-	}
-
-	@Override
-	protected void setupCircuits() {
-		// Circuit circuitA ===================================================================================================
+		Sounds
 		{
-			// build
-			{
-				CustomCircuit circuit = new CustomCircuit("circuitA");
-				add(circuit);
-				circuit.add("mySawTooth", new SawtoothOscillator());
-				circuit.add("highPass", new FilterHighPass());
-				circuit.add("M1", new Multiply());
-				
-				circuit.combineOutputs(); // connect outputs of all current elements to circuit output
-				
-				circuit.add("highPass", new FilterBandPass());
-				circuit.add("A1", new Add());
-				circuit.add("D1", new Divide());
-				
-				
-					// setup mySawTooth
-					getInputPort("circuitA.mySawTooth.Frequency")
-										.setup(50.0, 300.0, 10.0);
-	
-				
-	
-				
-	
-				
-	
-				
-	
-				
-	
-				
-			}
+			Oscillator "mySawTooth" (
+				output true
+				type sawTooth
+				min 50.0
+				maximum 10.000
+				default 300.0
+			)
+
+			PassFilter "highPass" (
+				output true
+				type high
+				frequency 5.000
+			)
+
+			BandPass "highPass" (
+				output false
+				lower_frequency 5.000
+				upper_frequency 10.000
+			)
+
+			Add "A1" (
+				output false
+			)
 			
-			//properties
+			Multiply "M1" (
+				output true
+			)
 			
-			// connect
-			{
+			Divide "D1" (
+				output false
+			)
+
+		}
+		
+		Properties
+		{	
+			"property1" soundX1 propertyName soundX2 propertyName 
+			"property2" soundX1 propertyName soundX2 propertyName 
+		}
+		
+		Connections
+		{
+		}
+	)
+}
+
+CircuitConnections 
+{
+	circuitA master
+}
+
+Controls
+{
+	Window "My Synthesizer"
+	{
+		width 1280
+		height 960
+	}
+	
+	Grid "master" {
+		Column 1.0
+		Row 0.25
+		Row 0.50
+		Row 0.25
+		
+		Grid "Global Controls" {
+			column 0
+			row 0
+			
+			Column 0.9
+			Column 0.1
+			Row 1.0
+			
+			rotaryKnob "Master Volume" (
+				column 1
+				row 0
+				connection (
+					type exponential
+					value 0.5
+					target "master.limiter"
+					property "Amplitude"
+				)
+				
+			)
+		}
+		
+		Grid "Circuit A" {
+			column 0
+			row 1
+			
+			Column 1.0
+			Row 0.5
+			Row 0.5
+			
+			
+			Grid "Controls" {
+				column 0
+				row 0
+				
+				Column 1.0
+				Column 1.0
+				Column 1.0
+				Row 1.0
+
+				rotaryKnob "OSCI Frequency" (
+					column 0
+					row 0
+					connection (
+						type linear
+						value 0.5
+						target "CircuitA.OSC1"
+						property "Frequency"
+					)
+				)
+
+				rotaryKnob "LAG" (
+					column 1
+					row 0
+					connection (
+						type exponential
+						value 0.5
+						target "CircuitA.LAG"
+						property "Input"
+					)
+				)
+
+				rotaryKnob "HPASS Frequency" (
+					column 2
+					row 0
+					connection (
+						type linear
+						value 0.5
+						target "CircuitA.HPASS"
+						property "Frequency"
+					)
+				)
+
 			}
-		}
-	}
 
-	@Override
-	protected void setupUI() {
-		super.setupUI();
-	
-		CustomGrid globalGrid = createGrid(new double[] { 
-			1.0
-		}, new double[] { 
-			0.25,
-			0.5,
-			0.25
-		 });
-	
+			
+			audioScope "" (
+				column 0
+				row 1
+				
+				connection (
+					target CircuitA
+				)	
+			)
 		
-		
-		// Global Controls
-		{
-			CustomGrid GlobalControlsGrid = createBorderedGrid("Global Controls", new double[] { 
-			1.0
-		}, new double[] { 
-			0.25,
-			0.5,
-			0.25
-		 });
-		 GlobalControlsGrid.add(createKnob("Master Volume", createExponentialModel("master.Amplitude")), 1, 0);
-		 globalGrid.add(GlobalControlsGrid, 0, 0);
-		}
-		// Circuit A
-		{
-			CustomGrid CircuitAGrid = createBorderedGrid("Circuit A", new double[] { 
-			1.0
-		}, new double[] { 
-			0.25,
-			0.5,
-			0.25
-		 });
-		 // Controls
-		 {
-		 	CustomGrid ControlsGrid = createBorderedGrid("Controls", new double[] { 
-		 	1.0,
-		 	1.0,
-		 	1.0
-		 }, new double[] { 
-		 	1.0
-		  });
-		 ControlsGrid.add(createKnob("OSCI Frequency", createLinearModel("CircuitA.OSC1.Frequency")), 0, 0);
-		 ControlsGrid.add(createKnob("LAG", createExponentialModel("CircuitA.LAG.Input")), 1, 0);
-		 ControlsGrid.add(createKnob("HPASS Frequency", createLinearModel("CircuitA.HPASS.Frequency")), 2, 0);
-		  	CircuitAGrid.add(ControlsGrid, 0, 0);
-		 }
-		 CircuitAGrid.add(createWaveView(new UnitOutputPort[]{getOutputPort("CircuitA.Output")}) ,0, 1);
-		 globalGrid.add(CircuitAGrid, 0, 1);
-		}
-	
-		// add global grid to root component
-		add(globalGrid.getPanel());
-	}
 
-	@Override
-	protected void setupCircuitConnections() {
-		connectCircuit("circuitA", "master");
-		connectCircuit("circuitB", "master");
+		}
 	}
 
 }
